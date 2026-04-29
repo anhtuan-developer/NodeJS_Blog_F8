7e84a1f83aa0d75b5a76c21a241fe900e2828d1c
Tuấn <tuanpx2k5@gmail.com>
2026-04-29 09:47:33 +0700

    Template Engine

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars");
+const port = 3000;
+
+// Template engine
+app.engine("hbs",handlebars.engine({
+  extname:".hbs"
+}));
+app.set("view engine","hbs");
+
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+app.get("/news", (req , res) =>{
+  res.render("news");
+})
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
