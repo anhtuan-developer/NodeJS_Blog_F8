f3a55b8135fced140e0469ecec78ce15e26e5fda
Tuấn <tuanpx2k5@gmail.com>
2026-05-07 08:23:51 +0700

    static & SCSS

--- src/server.js
+++ src/server.js
@@ -4,13 +4,15 @@ const app = express();
 const handlebars = require("express-handlebars");
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 // Template engine
 app.engine("hbs",handlebars.engine({
   extname:".hbs"
 }));
 app.set("view engine","hbs");
-
 app.set("views", path.join(__dirname, "resources/views"));
+
 
 app.get("/home", (req, res) => {
   res.render("home");
@@ -19,5 +21,5 @@ app.get("/news", (req , res) =>{
   res.render("news");
 })
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+  console.log(`http://localhost:${port}/home`);
 });
